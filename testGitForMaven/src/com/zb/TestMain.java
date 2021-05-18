e3683771a1b0dd84201b777acfc9151dc1201522
Magic870 <870983964@qq.com>
2021-05-18 11:20:46 +0800

    尝试第11次提交

--- testGitForMaven/src/com/zb/TestMain.java
+++ testGitForMaven/src/com/zb/TestMain.java
@@ -2,8 +2,6 @@ package com.zb;
 
 public class TestMain {
     public static void main(String[] args) {
-        System.out.println("hello git");
-
-        System.out.println("master");
+        System.out.println("FINAL this is master");
     }
 }
