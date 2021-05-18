a6a1b903b55ad94248330803de97471f1314eece
Magic870 <870983964@qq.com>
2021-05-18 11:02:59 +0800

    尝试第八次提交(oneBranch第二次提交(试冲突))

--- testGitForMaven/src/com/zb/TestMain.java
+++ testGitForMaven/src/com/zb/TestMain.java
@@ -3,5 +3,9 @@ package com.zb;
 public class TestMain {
     public static void main(String[] args) {
         System.out.println("hello git");
+
+        System.out.println("this is master changed");
+
+        System.out.println("this is oneBranch changed");
     }
 }
